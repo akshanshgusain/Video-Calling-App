e9cd67cd80029a7553e49b18cedfe1b258b549d8
akshanshgusain <akshanshgusain@gmail.com>
2022-11-10 16:52:57 +0530

    feat: added room, roomwebsocket, roomcreate, welcome handlers

--- internal/handlers/room.go
+++ internal/handlers/room.go
@@ -1,1 +1,39 @@
 package handlers
+
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/websocket/v2"
+	guuid "github.com/google/uuid"
+)
+
+func RoomCreate(c *fiber.Ctx) error {
+	return c.Redirect(fmt.Sprintf("/room/%s", guuid.New().String()))
+}
+
+func RoomWebsocket(c *websocket.Conn) {
+	uuid := c.Params("uuid")
+	if uuid == "" {
+		return
+	}
+
+	_, _, room := createOrGetRoom(uuid)
+
+	// TODO: finish later
+}
+
+func Room(c *fiber.Ctx) error {
+	uuid := c.Params("uuid")
+	if uuid == "" {
+		c.Status(400)
+		return nil
+	}
+
+	uuid, suuid, _ := createOrGetRoom(uuid)
+
+	// TODO: finish later
+}
+
+func createOrGetRoom(uuid string) (string, string, Room) {
+
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,54 @@
 package server
+
+import (
+	"flag"
+	"github.com/akshanshgusain/Video-Calling-App/internal/handlers"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/template/html"
+	"github.com/gofiber/websocket/v2"
+	"time"
+)
+
+var (
+	addr = flag.String("addr, ": "", os.Getenv(
+"PORT"
+), "")
+cert = flag.String("cert", "", "")
+key = flag.String("key", "", "")
+)
+
+
+func Run() error {
+	flag.Parse()
+
+	if *addr == ":" {
+		*addr = ":8080"
+	}
+
+	// Define Html engine
+	engine := html.New("./views", ".html")
+
+	// application instance
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+	app.Use(logger.New())
+	app.Use(cors.New())
+
+	// defining routes
+	app.Get("/", handlers.Welcome)
+	app.Get("/room/create", handlers.RoomCreate)
+	app.Get("/room/:uuid", handlers.Room)
+	app.Get("/room/:uuid/websocket", websocket.New(handlers.RoomWebsocket, websocket.Config{
+		HandshakeTimeout: 10 * time.Second,
+	}))
+	app.Get("/room/:uuid/chat", handlers.RoomChat)
+	app.Get("/room/:uuid/chat/websocket", websocket.New(handlers.RoomChatWebsocket))
+	app.Get("/room/:uuid/viewer/websocket", websocket.New(handlers.RoomViewerWebsocket))
+	app.Get("/stream/:ssuid", handlers.Stram)
+	app.Get("/stream/:ssuid/websocket", )
+	app.Get("/stream/ssuid/chat/websocket", )
+	app.Get("/stream/:ssuid/viewer/websocket", )
+}
