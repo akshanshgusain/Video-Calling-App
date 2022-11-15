0e3255310ca879a97b6784d6739de87762cce215
akshanshgusain <akshanshgusain@gmail.com>
2022-11-15 13:12:30 +0530

    feat(webrtc/peers): completed teh signalpeerconnections method

--- pkg/webrtc/peers.go
+++ pkg/webrtc/peers.go
@@ -44,7 +44,50 @@ func (p *Peers) RemoveTrack(t *webrtc.TrackLocalStaticRTP) {
 }
 
 func (p *Peers) SignalPeerConnections() {
+	p.ListLock.Lock()
+	defer func() {
+		p.ListLock.Unlock()
+		p.DispatchKeyFrame()
+	}()
 
+	attemptSync := func() (tryAgain bool) {
+		for i := range p.Connections {
+			if p.Connections[i].PeerConnection.ConnectionState() == webrtc.PeerConnectionStateClosed {
+				p.Connections = append(p.Connections[:i], p.Connections[i+1:]...)
+				log.Println("a", p.Connections)
+				return true
+			}
+
+			existingSenders := map[string]bool{}
+			for _, sender := range p.Connections[i].PeerConnection.GetSenders() {
+				if sender.Track() == nil {
+					continue
+				}
+				existingSenders[sender.Track().ID()] = true
+				if _, ok := p.trackLocals[sender.Track().ID()]; !ok {
+					if err := p.Connections[i].PeerConnection.RemoveTrack(Sender); err != nil {
+						return true
+					}
+				}
+			}
+
+			for _, receiver := range p.Connections[i].PeerConnection.GetReceivers() {
+				if receiver.Track() == nil {
+					continue
+				}
+				existingSenders[receiver.Track().ID()] = true
+			}
+
+			for trackID := range p.TrackLocals {
+				if _, ok := existingSenders[trackID]; !ok {
+					if _, err := p.Connections[i].PeerConnection.AddTrack(p.TrackLocals[trackID]); err != nil {
+						return true
+					}
+				}
+			}
+		}
+
+	}
 }
 
 func (p *Peers) DispatchKeyFrame() {
